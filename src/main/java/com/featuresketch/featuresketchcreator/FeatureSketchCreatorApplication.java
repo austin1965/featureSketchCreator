82b1a9da8bd55f3feb093e8dfd940c51852f87b0
austin1965 <83925251+austin1965@users.noreply.github.com>
2023-11-11 15:29:41 -0600

    Initial commit

---
+++ src/main/java/com/featuresketch/featuresketchcreator/FeatureSketchCreatorApplication.java
@@ -0,0 +1,14 @@
+package com.featuresketch.featuresketchcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureSketchCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureSketchCreatorApplication.class, args);
+		System.out.println("test");
+	}
+
+}
